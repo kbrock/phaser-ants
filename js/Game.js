5980382525ae71a4b70512fd070b6c212254ddeb
Keenan Brock <keenan@thebrocks.net>
2015-04-03 23:33:14 -0400

    pull out ant class

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,41 @@
 var AntGame = AntGame || {};
 
+//Ant
+AntGame.Ant = function(game) {
+  this.dead = false;
+  if(game)
+    this.createSprite(game);
+};
+
+AntGame.Ant.prototype = {
+  preload: function(game) {
+    game.load.spritesheet('ant', 'assets/images/ant.png', 16, 16);
+  },
+  createSprite: function(game) {
+    this.sprite = game.add.sprite(0, 0, 'ant');
+    this.sprite.animations.add('walk', [0, 1], 10, true);
+    this.sprite.animations.add('dead', [3], 1, true);
+  },
+  update: function(up, right, die) {
+    this.sprite.body.velocity.y = -50 * up;
+    this.sprite.body.velocity.x = 50 * right;
+
+    if(up != 0 || right != 0) {
+      this.sprite.play('walk');
+      this.dead = false;
+    } else {
+      if (die) {
+        this.dead = !this.dead;
+      }
+      if(this.dead)
+        this.sprite.play('dead')
+      else // reset to the walking animation (first frame)
+        this.sprite.animations.stop('walk', true);
+    }
+  }
+}
+
+
 //title screen
 AntGame.Game = function(){};
 
@@ -7,55 +43,33 @@ AntGame.Game.prototype = {
   init: function() {
   },
   preload: function() {
-    this.load.spritesheet('ant', 'assets/images/ant.png', 16, 16);
+    // move preload outside? Way to call AntGame.Ant.preload(this) ?
+    var a = new AntGame.Ant()
+    a.preload(this);
   },
   create: function() {
     this.game.stage.backgroundColor = '#d2691e';
-    this.player = this.game.add.sprite(0, 0, 'ant');
-    this.player.animations.add('walk', [0, 1], 10, true);
-    this.player.animations.add('dead', [3], 1, true);
-    this.game.physics.arcade.enable(this.player);
+    this.player = new AntGame.Ant(this);
 
+    this.game.physics.arcade.enable(this.player.sprite);
     //the camera will follow the player in the world
-    this.game.camera.follow(this.player);
+    this.game.camera.follow(this.player.sprite);
 
-    //move player with cursor keys
+    //action keys (move player with cursor keys, space kills him)
     this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.dead = false;
     this.die = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   },
+
   update: function() {
     //collision
     //this.game.physics.arcade.collide(this.player, this.blockedLayer);
     //this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
 
     //player movement
-    this.player.body.velocity.y = 0;
-    this.player.body.velocity.x = 0;
-
-    if(this.cursors.up.isDown) {
-      this.player.body.velocity.y -= 50;
-    }
-    else if(this.cursors.down.isDown) {
-      this.player.body.velocity.y += 50;
-    }
-    if(this.cursors.left.isDown) {
-      this.player.body.velocity.x -= 50;
-    } else if(this.cursors.right.isDown) {
-      this.player.body.velocity.x += 50;
-    }
-
-    if(this.player.body.velocity.x != 0 || this.player.body.velocity.y != 0) {
-      this.player.play('walk');
-      this.dead = false;
-    } else {
-      if (this.die.isDown) {
-        this.dead = !this.dead;
-      }
-      if(this.dead)
-        this.player.play('dead')
-      else // reset to the walking animation (first frame)
-        this.player.animations.stop('walk', true);
-    }
+    this.player.update(
+      this.cursors.up.isDown ? +1 : this.cursors.down.isDown ? -1 : 0,
+      this.cursors.left.isDown ? -1 : this.cursors.right.isDown ? +1 : 0,
+      this.die.isDown
+    )
   },
 };
